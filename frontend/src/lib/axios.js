c42fea16ed694eaed7fd01679733a49967eaa84c
Mahit <mahitsaxena44@gmail.com>
2026-01-12 02:17:21 +0530

    Swapped Vercel URL for Render URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // REPLACE "https://your-render-url.onrender.com" WITH YOUR ACTUAL RENDER LINK
 const BASE_URL = import.meta.env.MODE === "development" 
   ? "http://localhost:5001/api" 
-  : "https://streamify-j9vwfg5mq-mahitsaxena44-6372s-projects.vercel.app/api"; 
+  : "https://streamify-backend-qj9s.onrender.com/api"; 
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
